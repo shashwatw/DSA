cd6968a75a30ec3cf6afe373214e3f0ac1f881ea
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-03-27 09:33:10 +0530

    Leetcode peak index in a mountain array

---
+++ Binary Search/Leetcode_peak_index_in_mountain_array.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+
+        int s = 0;
+        int e = arr.size() - 1;
+        int mid;
+
+        while (s < e)
+        {
+            mid = s + (e - s) / 2;
+            if (arr[mid] < arr[mid + 1])
+            {
+                s = mid + 1;
+            }
+            else if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1])
+            {
+                return mid;
+            }
+
+            else if (arr[mid] > arr[mid + 1])
+            {
+                e = mid;
+            }
+        }
+        return mid;
+    }
+};
