91bc3424b3eae43c45c1ee4caa3a710a921a50b0
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-03-27 10:45:03 +0530

    Find exact sqrt in decimal upto precision

--- Binary Search/leetcode_69_squarerootx.cpp
+++ Binary Search/leetcode_69_squarerootx.cpp
@@ -1,31 +1,61 @@
-class Solution
-{
-public:
-    long long int mySqrt(int x)
-    {
-        long long int l, h, mid;
-        long long int ans = -1;
-        l = 0;
-        h = x;
-        mid = (l + h) / 2;
+#include <iostream>
+using namespace std;
 
-        while (l <= h)
+long long int mySqrt(int x)
+{
+    long long int l, h, mid;
+    long long int ans = -1;
+    l = 0;
+    h = x;
+    mid = (l + h) / 2;
+
+    while (l <= h)
+    {
+        mid = (l + h) / 2;
+        if (mid * mid < x)
         {
-            mid = (l + h) / 2;
-            if (mid * mid < x)
-            {
-                l = mid + 1;
-                ans = mid;
-            }
-            else if (mid * mid == x)
-            {
-                return (mid);
-            }
-            else
-            {
-                h = mid - 1;
-            }
+            l = mid + 1;
+            ans = mid;
         }
-        return (ans);
+        else if (mid * mid == x)
+        {
+            return (mid);
+        }
+        else
+        {
+            h = mid - 1;
+        }
     }
-};
+    return (ans);
+}
+
+double morePrecision(int n, int precision, int tempSol)
+{
+    double factor = 1;
+    double ans = tempSol;
+
+    for (int i = 0; i < precision; i++)
+    {
+        factor = factor / 10;
+
+        for (double j = ans; j * j < n; j = j + factor)
+        {
+            ans = j;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int n, precision;
+    cout << "Enter the number: " << endl;
+    cin >> n;
+    cout << "Enter the precision: " << endl;
+    cin >> precision;
+
+    int tempSol = mySqrt(n);
+    double ans = morePrecision(n, precision, tempSol);
+    cout << ans << endl;
+    return 0;
+}
