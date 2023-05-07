2e0e7fe09e0232d0bbc2c02a63bee7cfcb541d4e
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-05-07 12:12:32 +0530

    Iterative approach to GCD

---
+++ maths/gcd.cpp
@@ -0,0 +1,39 @@
+// Iterative approach to find GCD of two numbers
+
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+    {
+        return b;
+    }
+    else if (b == 0)
+    {
+        return a;
+    }
+
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a = a - b;
+        }
+        else
+        {
+            b = b - a;
+        }
+    }
+}
+
+int main()
+{
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "GCD of " << a << " and " << b << " is " << gcd(a, b);
+
+    return 0;
+}
