e8ab51dde8cb452fc9006b4412af37dd4aab8eac
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-05-09 10:42:11 +0530

    GFG Kth Smallest Element

---
+++ Arrays/GFG_KthSmallestElement.cpp
@@ -0,0 +1,49 @@
+// https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1
+
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+#include <stdlib.h>
+
+class Solution
+{
+public:
+    int kthSmallest(int arr[], int l, int r, int k)
+    {
+
+        sort(arr, arr + r + 1);
+
+        int smallest = arr[k - 1];
+
+        return smallest;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int test_case;
+    cin >> test_case;
+    while (test_case--)
+    {
+        int number_of_elements;
+        cin >> number_of_elements;
+        int a[number_of_elements];
+
+        for (int i = 0; i < number_of_elements; i++)
+            cin >> a[i];
+
+        int k;
+        cin >> k;
+        Solution ob;
+        cout << ob.kthSmallest(a, 0, number_of_elements - 1, k) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
