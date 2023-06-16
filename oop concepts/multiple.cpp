746c14a8e4ed56e9ba2cf81ff2fafc91487053cc
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-06-16 07:38:43 +0530

    Multiple Inheritance

---
+++ oop concepts/multiple.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+    int age;
+    int weight;
+
+public:
+    void bark()
+    {
+        cout << "Barking" << endl;
+    }
+};
+
+class Human
+{
+public:
+    string color;
+
+public:
+    void speak()
+    {
+        cout << "Speaking" << endl;
+    }
+};
+
+// multiple inheritance
+
+class Hybrid : public Animal, public Human
+{
+};
+
+int main()
+{
+    Hybrid obj;
+
+    obj.speak();
+    obj.bark();
+    cout << obj.color << endl;
+    cout << obj.age << endl;
+
+    return 0;
+}
