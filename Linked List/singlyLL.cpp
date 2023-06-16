03b87703870811f5523de0ae0730e46a83410d07
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-06-16 10:55:34 +0530

    Singly Linked List

---
+++ Linked List/singlyLL.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // destructor
+    ~Node()
+    {
+        int value = this->data;
+        // memory free
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+        cout << "Memory is free for node with data " << value << endl;
+    }
+};
+
+void insertAtHead(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+
+void insertAtPosition(Node *&tail, Node *&head, int position, int d)
+{
+    if (position == 1)
+    {
+        insertAtHead(head, d);
+    }
+    else
+    {
+        Node *temp = head;
+        int cnt = 1;
+        while (cnt < position - 1)
+        {
+            temp = temp->next;
+            cnt++;
+        }
+
+        if (temp->next == NULL)
+        {
+            insertAtTail(tail, d);
+            return;
+        }
+
+        Node *nodeToInsert = new Node(d);
+
+        nodeToInsert->next = temp->next;
+        temp->next = nodeToInsert;
+    }
+}
+
+void deleteNode(int position, Node *&head, Node *&tail)
+{
+    // deleting first node
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        // memory free
+        temp->next = NULL;
+        delete temp;
+    }
+    else
+    {
+        // deleting any middle node or last node
+        Node *curr = head;
+        Node *prev = NULL;
+
+        int cnt = 1;
+        while (cnt < position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+        if (curr->next == NULL)
+        {
+            tail = prev;
+        }
+
+        prev->next = curr->next;
+        curr->next = NULL;
+        delete curr;
+    }
+}
+
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // created a new node
+    Node *node1 = new Node(10);
+    // cout << node1->data << endl;
+    // cout << node1->next << endl;
+
+    // pointed head to newnode
+    Node *head = node1;
+    Node *tail = node1;
+    print(head);
+
+    // insertAtHead(head, 12);
+    // print(head);
+
+    // insertAtHead(head, 15);
+    // print(head);
+
+    insertAtTail(tail, 12);
+    print(head);
+
+    insertAtTail(tail, 15);
+    print(head);
+
+    insertAtPosition(tail, head, 3, 22);
+    print(head);
+
+    insertAtPosition(tail, head, 1, 99);
+    print(head);
+
+    insertAtPosition(tail, head, 6, 100);
+    print(head);
+
+    cout << "Head: " << head->data << endl;
+    cout << "Tail: " << tail->data << endl;
+
+    cout << endl
+         << endl;
+
+    deleteNode(1, head, tail);
+    print(head);
+    cout << "Head: " << head->data << endl;
+    cout << "Tail: " << tail->data << endl;
+
+    deleteNode(5, head, tail);
+    print(head);
+
+    cout << "Head: " << head->data << endl;
+    cout << "Tail: " << tail->data << endl;
+
+    return 0;
+}
