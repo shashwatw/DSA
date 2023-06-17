74a798424c84783506aeec3de2b57815269d2097
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-06-17 12:26:58 +0530

    Leetcode 206 Reverse Linked List using Recursive approach

--- Linked List/Leetcode_206_Reverse-Linked-List.cpp
+++ Linked List/Leetcode_206_Reverse-Linked-List.cpp
@@ -1,18 +1,37 @@
 class Solution
 {
 public:
+    // Recursive Approach function
+    void reverse(ListNode *&head, ListNode *curr, ListNode *prev)
+    {
+        // base case
+        if (curr == NULL)
+        {
+            head = prev;
+            return;
+        }
+
+        ListNode *forward = curr->next;
+        reverse(head, forward, curr);
+
+        curr->next = prev;
+    }
+
     ListNode *reverseList(ListNode *head)
     {
-        if (head == NULL || head->next == NULL)
+
+        // Iterative aproach
+        /*
+        if(head == NULL || head->next == NULL)
         {
             return head;
         }
 
-        ListNode *prev = NULL;
-        ListNode *curr = head;
-        ListNode *forward = NULL;
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* forward = NULL;
 
-        while (curr != NULL)
+        while(curr != NULL)
         {
             forward = curr->next;
             curr->next = prev;
@@ -21,5 +40,13 @@ public:
         }
 
         return prev;
+        */
+
+        // Recursive Approach
+
+        ListNode *curr = head;
+        ListNode *prev = NULL;
+        reverse(head, curr, prev);
+        return head;
     }
 };
