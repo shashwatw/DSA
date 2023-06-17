52bee51b7f76b20aae4bdf7bb52144db30a0aa88
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-06-17 12:59:52 +0530

    Leetcode 876 Middle Of The Linked List using 2 pointer appproach...fast and slow

--- Linked List/Leetcode_876_Middle-of-the-linked-list.cpp
+++ Linked List/Leetcode_876_Middle-of-the-linked-list.cpp
@@ -30,3 +30,41 @@ public:
         return temp1;
     }
 };
+
+// Approach 2 Using fast and slow pointers
+class Solution
+{
+public:
+    ListNode *getMiddle(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+
+        // 2 nodes
+        if (head->next->next == NULL)
+        {
+            return head->next;
+        }
+
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+
+        while (fast != NULL)
+        {
+            fast = fast->next;
+            if (fast != NULL)
+            {
+                fast = fast->next;
+            }
+            slow = slow->next;
+        }
+        return slow;
+    }
+
+    ListNode *middleNode(ListNode *head)
+    {
+        return getMiddle(head);
+    }
+};
