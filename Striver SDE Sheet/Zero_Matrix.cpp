fd2ff2d2a86cc9a55c3b4c38e26f63403331a406
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-08-08 20:08:28 +0530

    Zero Matrix Optimal Approach

--- Striver SDE Sheet/Zero_Matrix.cpp
+++ Striver SDE Sheet/Zero_Matrix.cpp
@@ -33,3 +33,62 @@ vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m)
 
     return matrix;
 }
+
+// Optimal Approach
+
+#include <bits/stdc++.h>
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m)
+{
+    //   int row[n] = { 0 };
+    //   int col[m] = { 0 };
+    int col0 = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                // mark the ith row
+                matrix[i][0] = 0;
+
+                if (j != 0)
+                {
+                    matrix[0][j] = 0;
+                }
+                else
+                {
+                    col0 = 0;
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < m; j++)
+        {
+            if (matrix[0][j] == 0 || matrix[i][0] == 0)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if (matrix[0][0] == 0)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[0][j] = 0;
+        }
+    }
+
+    if (col0 == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i][0] = 0;
+        }
+    }
+
+    return matrix;
+}
