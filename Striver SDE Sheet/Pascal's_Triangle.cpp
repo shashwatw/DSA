8e6b3b9019622c752b498901f2025a68392efad5
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-08-09 08:43:07 +0530

    Pascal's Triangle Codestudio

---
+++ Striver SDE Sheet/Pascal's_Triangle.cpp
@@ -0,0 +1,25 @@
+// https://www.codingninjas.com/studio/problems/print-pascal-s-triangle_6917910?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+vector<int> generateRow(int row)
+{
+    long long ans = 1;
+    vector<int> ansRow;
+    ansRow.push_back(1);
+    for (int col = 1; col < row; col++)
+    {
+        ans = ans * (row - col);
+        ans = ans / (col);
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+
+vector<vector<int>> pascalTriangle(int N)
+{
+    vector<vector<int>> ans;
+    for (int i = 1; i <= N; i++)
+    {
+        ans.push_back(generateRow(i));
+    }
+    return ans;
+}
