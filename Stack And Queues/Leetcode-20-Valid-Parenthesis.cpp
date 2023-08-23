cdba9b41a2df3ba21cc9edc02d3907afe399209d
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-08-23 11:13:36 +0530

    Leetcode 20 Valid Parenthesis

---
+++ Stack And Queues/Leetcode-20-Valid-Parenthesis.cpp
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/valid-parentheses/description/
+
+#include<stack>
+#include<string>
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i< s.size(); i++)
+        {
+            if((s[i]=='(')||(s[i]=='[')||(s[i]=='{'))
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if(st.empty()) return false;
+                
+                char c = st.top();
+                st.pop();
+                if((s[i]==')' && c == '(')||(s[i]==']' && c == '[')||(s[i]=='}' && c == '{'))
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }   
+        return st.empty();
+    }
+};
