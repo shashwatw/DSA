d1d342dad082cc5a3d33355bb79dda530897673c
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-08-25 14:32:45 +0530

    Leetcode 151 Reverse words in a string using Stringstream

--- Strings/Leetcode-151-Reverse_words_in_a_string.cpp
+++ Strings/Leetcode-151-Reverse_words_in_a_string.cpp
@@ -57,3 +57,27 @@ public:
         return final_ans;
     }
 };
+
+
+
+//2nd version using stringstream
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> words;
+        stringstream ss(s);
+        string word;
+        while (ss >> word) {
+            words.push_back(word);
+        }
+        
+        string reversed;
+        for (int i = words.size() - 1; i >= 0; i--) {
+            reversed += words[i];
+            if (i > 0) {
+                reversed += " "; 
+            }
+        }
+        return reversed;
+    }
+};
