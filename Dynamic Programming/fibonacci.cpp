f379f075f76fd42ee614afd618705cdce6fad026
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-09-24 12:08:21 +0530

    DP Fibonacci Tabulation

--- Dynamic Programming/fibonacci.cpp
+++ Dynamic Programming/fibonacci.cpp
@@ -1,3 +1,6 @@
+
+// Memoization
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -20,5 +23,30 @@ int main()
     cin >> n;
     vector<int> dp(n + 1, -1);
     cout << f(n, dp) << endl;
+    return 0;
+}
+
+// Tabulation
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int n)
+{
+    vector<int> dp(n + 1, -1);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i < n + 1; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << f(n) << endl;
     return 0;
 }
