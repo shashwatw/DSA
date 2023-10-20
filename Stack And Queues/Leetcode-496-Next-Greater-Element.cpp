f75df3f9dd5fd8bfd8a77ef373eb1925d68ea2c7
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-10-20 16:36:14 +0530

    Leetcode 496 Next Greater Element

---
+++ Stack And Queues/Leetcode-496-Next-Greater-Element.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/next-greater-element-i/
+
+class Solution
+{
+public:
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+    {
+        stack<int> st;
+        unordered_map<int, int> mp;
+        int n = nums2.size();
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int ele = nums2[i];
+            while (!st.empty() && st.top() <= ele)
+            {
+                st.pop();
+            }
+            int res = (st.empty()) ? -1 : st.top();
+
+            mp.insert({ele, res});
+            st.push(ele);
+        }
+        vector<int> ans;
+        for (auto x : nums1)
+        {
+            ans.push_back(mp[x]);
+        }
+        return ans;
+    }
+};
