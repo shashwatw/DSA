ab7a696cedda32a9d91fc3846b768ec703433a5d
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-12-01 08:37:17 +0530

    GFG Detect cycle in an undirected graph using DFS

--- Graphs/GFG_Detect-cycle-in-an-undirected-graph.cpp
+++ Graphs/GFG_Detect-cycle-in-an-undirected-graph.cpp
@@ -1,4 +1,5 @@
 // https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+// BFS
 
 //{ Driver Code Starts
 #include <bits/stdc++.h>
@@ -46,6 +47,82 @@ public:
             {
                 if (detect(i, adj, vis))
                     return true;
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+// DFS Approach
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+private:
+    bool dfs(int node, int parent, int vis[], vector<int> adj[])
+    {
+        vis[node] = 1;
+        // visit adjacent nodes
+        for (auto adjacentNode : adj[node])
+        {
+            // unvisited adjacent node
+            if (!vis[adjacentNode])
+            {
+                if (dfs(adjacentNode, node, vis, adj) == true)
+                    return true;
+            }
+            // visited node but not a parent node
+            else if (adjacentNode != parent)
+                return true;
+        }
+        return false;
+    }
+
+public:
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[])
+    {
+        int vis[V] = {0};
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                if (dfs(i, -1, vis, adj) == true)
+                {
+                    return true;
+                }
             }
         }
         return false;
