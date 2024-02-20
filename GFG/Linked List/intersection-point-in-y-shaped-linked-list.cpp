a659f6339ba1063a3052b2d04c09110488fdef51
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-02-20 15:50:54 +0530

    intersection point in Y shaped linked list

---
+++ GFG/Linked List/intersection-point-in-y-shaped-linked-list.cpp
@@ -0,0 +1,235 @@
+// https://www.geeksforgeeks.org/batch/test-series-bundle/track/amazon-linkedlists/problem/intersection-point-in-y-shapped-linked-lists
+
+//{ Driver Code Starts
+#include <iostream>
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int intersectPoint(struct Node *head1, struct Node *head2);
+
+vector<int> take(int n)
+{
+    vector<int> ans(n);
+    for (int i = 0; i < n; i++)
+        cin >> ans[i];
+    return ans;
+}
+
+Node *inputList(int size, vector<int> v)
+{
+    if (size == 0)
+        return NULL;
+
+    int val = v[0];
+
+    Node *head = new Node(val);
+    Node *tail = head;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        val = v[i + 1];
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    srand(time(0));
+    int T, n1, n2, n3;
+
+    cin >> T;
+    while (T--)
+    {
+        cin >> n1 >> n2 >> n3;
+
+        int p = rand() % 3;
+
+        vector<int> v1 = take(n1);
+        vector<int> v2 = take(n2);
+        vector<int> v3 = take(n3);
+
+        Node *head1 = NULL;
+        Node *head2 = NULL;
+        Node *common = NULL;
+
+        if (p == 0)
+        {
+            common = inputList(n3, v3);
+            head1 = inputList(n1, v1);
+            head2 = inputList(n2, v2);
+        }
+        else if (p == 1)
+        {
+
+            head1 = inputList(n1, v1);
+            common = inputList(n3, v3);
+            head2 = inputList(n2, v2);
+        }
+        else
+        {
+
+            head1 = inputList(n1, v1);
+            head2 = inputList(n2, v2);
+            common = inputList(n3, v3);
+        }
+
+        Node *temp = head1;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL)
+            temp->next = common;
+
+        temp = head2;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL)
+            temp->next = common;
+
+        cout << intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+// Function to find intersection point in Y shaped Linked Lists.
+//  int intersectPoint(Node* head1, Node* head2)
+//  {
+//      // Your Code Here
+//      Node* temp1 = head1;
+//      Node* temp2 = head2;
+//      int l1 = 1;
+//      int l2 = 1;
+
+//     while(temp1 != NULL)
+//     {
+//         l1++;
+//         temp1 = temp1->next;
+//     }
+
+//     while(temp2 != NULL)
+//     {
+//         l2++;
+//         temp2 = temp2->next;
+//     }
+
+//     int diff = abs(l1-l2);
+//     int flag = 0;
+
+//     temp1 = head1;
+//     temp2 = head2;
+//     if(l1>l2)
+//     {
+//         while(flag != diff)
+//         {
+//             temp1 = temp1->next;
+//             flag++;
+//         }
+
+//         int i = 0;
+//         int j = 0;
+
+//         while(temp1 != NULL && temp2 != NULL)
+//         {
+//             if(temp1->next == temp2->next)
+//             {
+//                 return temp1->next->data;
+//             }
+//             else
+//             {
+//                 temp1 = temp1->next;
+//                 temp2 = temp2->next;
+//             }
+//         }
+//     }
+//     else
+//     {
+//         while(flag != diff)
+//         {
+//             temp2 = temp2->next;
+//             flag++;
+//         }
+//     }
+//     return -1;
+// }
+
+int intersectPoint(Node *head1, Node *head2)
+{
+    // Your Code Here
+    Node *temp1 = head1;
+    Node *temp2 = head2;
+    int l1 = 0;
+    int l2 = 0;
+
+    while (temp1 != NULL)
+    {
+        l1++;
+        temp1 = temp1->next;
+    }
+
+    while (temp2 != NULL)
+    {
+        l2++;
+        temp2 = temp2->next;
+    }
+
+    int diff = abs(l1 - l2);
+
+    temp1 = head1;
+    temp2 = head2;
+
+    if (l1 > l2)
+    {
+        for (int i = 0; i < diff; i++)
+        {
+            temp1 = temp1->next;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < diff; i++)
+        {
+            temp2 = temp2->next;
+        }
+    }
+
+    while (temp1 != NULL && temp2 != NULL)
+    {
+        if (temp1 == temp2)
+        {
+            return temp1->data;
+        }
+
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+
+    return -1;
+}
