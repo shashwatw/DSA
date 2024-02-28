979cac06a3a610062bac556cf3a3b0a3ce4cba1a
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-02-28 10:24:03 +0530

    Linked list sorted alternatively

---
+++ GFG/Linked List/linked-list-sorted-alternatively.cpp
@@ -0,0 +1,102 @@
+// https://www.geeksforgeeks.org/batch/test-series-bundle/track/amazon-linkedlists/problem/linked-list-that-is-sorted-alternatingly
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void sort(Node **head);
+
+void append(struct Node **headRef, struct Node **tailRef, int newData)
+{
+    struct Node *new_node = new Node(newData);
+    struct Node *last = *headRef;
+    if (*headRef == NULL)
+    {
+        *headRef = new_node;
+        *tailRef = *headRef;
+        return;
+    }
+
+    (*tailRef)->next = new_node;
+    *tailRef = (*tailRef)->next;
+}
+
+// A utility function to print a linked list
+void printList(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        struct Node *head = NULL;
+        struct Node *tail = NULL;
+        int n, k;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> k;
+            append(&head, &tail, k);
+        }
+        sort(&head);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+/*Structure of Node of the linked list is as
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x){
+        data =x;
+        next = NULL;
+    }
+};
+*/
+// your task is to complete this function
+void sort(Node **head)
+{
+    // Code here
+    vector<int> v;
+    Node *curr = *head;
+    while (curr)
+    {
+        v.push_back(curr->data);
+        curr = curr->next;
+    }
+
+    sort(v.begin(), v.end());
+    Node *temp = *head;
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        temp->data = v[i];
+        temp = temp->next;
+    }
+}
