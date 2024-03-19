0c26164942c564fb94001ebcf19f913304f4c4a9
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-03-19 11:39:44 +0530

    Vertical Order Traversal of a binary tree

---
+++ GFG/Flipkart/Trees/vertical-traversal-of-binary-tree.cpp
@@ -0,0 +1,178 @@
+// https://www.geeksforgeeks.org/batch/test-series-bundle/track/FTS-tree/problem/print-a-binary-tree-in-vertical-order
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+};
+// Utility function to create a new Tree Node
+Node *newNode(int val)
+{
+    Node *temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Function for Inorder Traversal
+void printInorder(Node *root)
+{
+    if (!root)
+        return;
+
+    printInorder(root->left);
+    cout << root->data << " ";
+    printInorder(root->right);
+}
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Function to find the vertical order traversal of Binary Tree.
+    vector<int> verticalOrder(Node *root)
+    {
+        // Your code here
+        map<int, map<int, vector<int>>> nodes;
+        queue<pair<Node *, pair<int, int>>> todo;
+
+        todo.push({root, {0, 0}});
+
+        while (!todo.empty())
+        {
+            auto p = todo.front();
+            todo.pop();
+            Node *node = p.first;
+
+            int x = p.second.first;  // level
+            int y = p.second.second; // node->data
+
+            nodes[x][y].push_back(node->data);
+
+            if (node->left != NULL)
+            {
+                todo.push({node->left, {x - 1, y + 1}});
+            }
+            if (node->right != NULL)
+            {
+                todo.push({node->right, {x + 1, y + 1}});
+            }
+        }
+
+        vector<int> ans;
+
+        for (auto p : nodes)
+        {
+            for (auto q : p.second)
+            {
+                for (auto r : q.second)
+                {
+                    ans.push_back(r);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--)
+    {
+        string s;
+        getline(cin, s);
+        // string c;
+        // getline(cin,c);
+        Solution obj;
+        Node *root = buildTree(s);
+
+        vector<int> res = obj.verticalOrder(root);
+        for (int i : res)
+            cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
