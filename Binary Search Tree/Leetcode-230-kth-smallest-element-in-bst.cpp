8411423a9d6e6f6040638c11cc756632f0a6d56e
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-03-28 10:25:12 +0530

    Leetcode 230 Kth smallest element in a BST

---
+++ Binary Search Tree/Leetcode-230-kth-smallest-element-in-bst.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+class Solution
+{
+public:
+    void solve(TreeNode *root, int &cnt, int &ans, int k)
+    {
+        if (root == NULL)
+            return;
+
+        // left, root, right
+        solve(root->left, cnt, ans, k);
+        cnt++;
+        if (cnt == k)
+        {
+            ans = root->val;
+            return;
+        }
+        solve(root->right, cnt, ans, k);
+    }
+
+    int kthSmallest(TreeNode *root, int k)
+    {
+
+        int cnt = 0;
+        int ans;
+        solve(root, cnt, ans, k);
+        return ans;
+    }
+};
