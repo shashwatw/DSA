0fb563420869fb7571d3f5adf4b6b631da6855a2
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-04-11 13:32:45 +0530

    Leetcode 402 Remove K Digits

---
+++ Stack And Queues/Leetcode-402-Remove-K-Digits.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/remove-k-digits/
+
+class Solution
+{
+public:
+    string removeKdigits(string num, int k)
+    {
+        vector<char> stk;
+        string ans;
+        for (char c : num)
+        {
+            while (k > 0 && !stk.empty() && stk.back() > c)
+            {
+                stk.pop_back();
+                k--;
+            }
+            stk.push_back(c);
+        }
+        while (k > 0)
+        {
+            stk.pop_back();
+            k--;
+        }
+        for (char c : stk)
+        {
+            if (ans.empty() && c == '0')
+                continue;
+            ans.push_back(c);
+        }
+        return ans.empty() ? "0" : ans;
+    }
+};
