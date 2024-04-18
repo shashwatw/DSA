99a60f81861e8b03421457a5ef239ba09d7e06d3
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-04-18 21:29:58 +0530

    Find kth element of spiral matrix

---
+++ GFG/Amazon/Matrix/Find-Kth-element-of-spiral-matrix.cpp
@@ -0,0 +1,93 @@
+// https://www.geeksforgeeks.org/batch/test-series-bundle/track/amazon-matrix/problem/find-nth-element-of-spiral-matrix
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    /*You are required to complete this method*/
+    int findK(int matrix[MAX][MAX], int n, int m, int k)
+    {
+        // Your code goes here.
+        int left = 0;
+        int top = 0;
+        int right = m - 1;
+        int bottom = n - 1;
+
+        vector<int> ans;
+
+        while (top <= bottom && left <= right)
+        {
+            for (int i = left; i <= right; i++)
+            {
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++)
+            {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom)
+            {
+                for (int i = right; i >= left; i--)
+                {
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right)
+            {
+                for (int i = bottom; i >= top; i--)
+                {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        int flag = 0;
+        for (int i = 0; i < ans.size(); i++)
+        {
+            flag++;
+            if (flag == k)
+            {
+                return ans[k - 1];
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int n, m;
+        int k = 0;
+        // cin>>k;
+        cin >> n >> m >> k;
+        int a[MAX][MAX];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cin >> a[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.findK(a, n, m, k) << endl;
+    }
+}
+// } Driver Code Ends
