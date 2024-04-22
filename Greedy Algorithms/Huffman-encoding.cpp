f9d988b2087d0201c1e7a71613b8c95eb8d7da27
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-04-23 00:06:56 +0530

    HuffMann Encoding

---
+++ Greedy Algorithms/Huffman-encoding.cpp
@@ -0,0 +1,77 @@
+//! https://www.geeksforgeeks.org/problems/huffman-encoding3345/1
+
+class Node
+{
+
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class cmp
+{
+public:
+    bool operator()(Node *a, Node *b)
+    {
+        return a->data > b->data;
+    }
+};
+
+void traverse(Node *root, vector<string> &ans, string temp)
+{
+    if (root->left == NULL && root->right == NULL)
+    {
+        ans.push_back(temp);
+        return;
+    }
+
+    traverse(root->left, ans, temp + '0');
+    traverse(root->right, ans, temp + '1');
+}
+
+class Solution
+{
+public:
+    vector<string> huffmanCodes(string S, vector<int> f, int N)
+    {
+        // Code here
+        priority_queue<Node *, vector<Node *>, cmp> pq;
+
+        for (int i = 0; i < N; i++)
+        {
+            Node *temp = new Node(f[i]);
+            pq.push(temp);
+        }
+
+        while (pq.size() > 1)
+        {
+            Node *left = pq.top();
+            pq.pop();
+
+            Node *right = pq.top();
+            pq.pop();
+
+            Node *newNode = new Node(left->data + right->data);
+            newNode->left = left;
+            newNode->right = right;
+
+            pq.push(newNode);
+        }
+
+        string temp = "";
+        Node *root = pq.top();
+        vector<string> ans;
+
+        traverse(root, ans, temp);
+
+        return ans;
+    }
+};
