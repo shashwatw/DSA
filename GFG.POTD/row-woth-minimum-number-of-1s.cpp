c385c48c6eddc6a93794b28376dedee2120146b4
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-04-22 16:02:53 +0530

    Row with minimum number of 1s

---
+++ GFG.POTD/row-woth-minimum-number-of-1s.cpp
@@ -0,0 +1,62 @@
+// https://www.geeksforgeeks.org/problems/row-with-minimum-number-of-1s5430/1
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    int minRow(int n, int m, vector<vector<int>> arr)
+    {
+        // code here
+        int lastCount = INT_MAX;
+        int low = 1;
+        for (int i = 0; i < n; i++)
+        {
+            int cnt = 0;
+            for (int j = 0; j < m; j++)
+            {
+                if (arr[i][j] == 1)
+                {
+                    cnt++;
+                }
+            }
+            if (cnt < lastCount)
+            {
+                low = i + 1;
+            }
+            lastCount = min(lastCount, cnt);
+        }
+        return low;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, M;
+        cin >> N >> M;
+        vector<vector<int>> A(N, vector<int>(M));
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                cin >> A[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.minRow(N, M, A) << "\n";
+    }
+}
+// } Driver Code Ends
