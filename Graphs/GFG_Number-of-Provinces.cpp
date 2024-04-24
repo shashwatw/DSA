a6121268ae630e642d4d9a24074d842df51afb0f
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-04-25 00:28:49 +0530

    LC547 Number of provinces

--- Graphs/GFG_Number-of-Provinces.cpp
+++ Graphs/GFG_Number-of-Provinces.cpp
@@ -1,4 +1,4 @@
-// https://practice.geeksforgeeks.org/problems/number-of-provinces/1
+//^ https://practice.geeksforgeeks.org/problems/number-of-provinces/1
 
 // SC - O(n) + O(n);
 // TC - O(n) + O(V+2E) {nearly linear TC}
@@ -89,3 +89,60 @@ int main()
     return 0;
 }
 // } Driver Code Ends
+
+//* LEETCODE - Striver Approach
+
+//& by DFS, humne first adjacency matrix ko convert kiya in adjacency list
+//& then we have made a new vis vector.
+//& when disconnection is obtained we will call dfs and also increment a counter to track disconnections
+//& Return counts as final they are the total disconnections or provinces obtained
+
+class Solution
+{
+public:
+    void dfs(int node, vector<int> adjLs[], vector<int> &vis)
+    {
+        vis[node] = 1;
+
+        for (auto it : adjLs[node])
+        {
+            if (!vis[it])
+            {
+                dfs(it, adjLs, vis);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>> &isConnected)
+    {
+        int V = isConnected[0].size();
+
+        vector<int> adjLs[V];
+
+        for (int i = 0; i < V; i++)
+        {
+            for (int j = 0; j < V; j++)
+            {
+                if (isConnected[i][j] == 1 && i != j)
+                {
+                    adjLs[i].push_back(j);
+                    adjLs[j].push_back(i);
+                }
+            }
+        }
+
+        vector<int> vis(V, 0);
+
+        int cnt = 0;
+
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                cnt++;
+                dfs(i, adjLs, vis);
+            }
+        }
+        return cnt;
+    }
+};
