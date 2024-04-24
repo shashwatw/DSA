38e743d3ee48504489763691cbaf3143274fd67b
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-04-24 20:50:31 +0530

    All approaches of DP for Paths to reach origin

--- Dynamic Programming/Paths-to-reach-origin.cpp
+++ Dynamic Programming/Paths-to-reach-origin.cpp
@@ -1,31 +1,94 @@
 //& https://www.geeksforgeeks.org/problems/paths-to-reach-origin3850/1
 
+//^ Approach 1 -> Brute Approach - Recursive (TLE Here)
+//* Here recursion will start from arr[x][y] which is bottom most and check for each uske upar and right... Only
+//* only two directions mein ja skte h for origin i.e up and right
+//* yaha arr[x][y] ke upar and right dono ki ways add krte krte jaenge....
+
+//~ CODE
+
 // User function Template for C++
 class Solution
 {
 public:
-    int mod = 1e9 + 7;
-
-    int help(int x, int y, vector<vector<int>> &dp)
+    int ways(int x, int y)
     {
-
-        if (x == 0 and y == 0)
+        // code here.
+        if (x == 0 || y == 0)
             return 1;
-        if (x < 0 or y < 0)
-            return 0;
-        if (dp[x][y] != -1)
-            return dp[x][y];
 
-        int a = help(x - 1, y, dp);
-        int b = help(x, y - 1, dp);
+        int res = 0;
 
-        return dp[x][y] = (a + b) % mod;
+        res += ways(x - 1, y);
+        res += ways(x, y - 1);
+
+        return res;
+    }
+};
+
+//^ Approach 2 --> Memoization Approach....
+//* yaha create a 2D vector of size [x+1][y+1] and initialise with -1;
+//* then store krte rho 2D vector mein and calls krte rho recursive taaki jab -1 na ho col then
+//* fetch krlo already prefilled values from the 2D array.
+
+//~ CODE:
+// User function Template for C++
+// User function Template for C++
+class Solution
+{
+public:
+    int countUtil(int x, int y, vector<vector<long long>> &memo)
+    {
+        if (x == 0 || y == 0)
+            return 1;
+
+        if (memo[x][y] != -1)
+            return memo[x][y];
+
+        memo[x][y] = countUtil(x - 1, y, memo) + countUtil(x, y - 1, memo);
+        memo[x][y] = memo[x][y] % 1000000007;
+
+        return memo[x][y];
     }
 
     int ways(int x, int y)
     {
-        vector<vector<int>> dp(x + 1, vector<int>(y + 1, -1));
+        // code here.
+        vector<vector<long long>> memo(x + 1, vector<long long>(y + 1, -1));
 
-        return help(x, y, dp);
+        return countUtil(x, y, memo);
+    }
+};
+
+//^ Approach 3 - Tabulation (Iterative way)
+//* In this way humne ek 2d array banaaya and first row and first col ko 1 se initialise kiya
+//* becoz 1 hi path hoga.
+//* Simply start by arr[1][1] and go till arr[x][y] basically it is TOP DOWN Approach
+
+//~CODE:
+// User function Template for C++
+class Solution
+{
+public:
+    int countUtil(int x, int y, vector<vector<long long>> &memo)
+    {
+        if (x == 0 || y == 0)
+            return 1;
+
+        if (memo[x][y] != -1)
+            return memo[x][y];
+
+        memo[x][y] = countUtil(x - 1, y, memo) + countUtil(x, y - 1, memo);
+        memo[x][y] = memo[x][y] % 1000000007;
+
+        return memo[x][y];
+    }
+
+    int ways(int x, int y)
+    {
+        // code here.
+        vector<vector<long long>> memo(x + 1, vector<long long>(y + 1, -1));
+
+        return countUtil(x, y, memo);
     }
 };
