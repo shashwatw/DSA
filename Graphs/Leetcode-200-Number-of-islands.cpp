49a94b89187af24dabbc9e553f1ea9f991b25393
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-04-25 19:15:10 +0530

    LC 200 Num of Islands using DFS

--- Graphs/Leetcode-200-Number-of-islands.cpp
+++ Graphs/Leetcode-200-Number-of-islands.cpp
@@ -97,3 +97,60 @@ public:
         return cnt;
     }
 };
+
+//^ Approach 2 Using DFS (Also Contains trick for traversing all 4 neighbours)
+
+//~ Code
+
+class Solution
+{
+private:
+    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        vis[row][col] = 1;
+
+        // Define the directions to explore: up, down, left, right
+        int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        // Traverse in all four directions
+        for (int i = 0; i < 4; i++)
+        {
+            int nrow = row + dirs[i][0];
+            int ncol = col + dirs[i][1];
+
+            // Check if the new cell is within bounds and is land ('1')
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                !vis[nrow][ncol] && grid[nrow][ncol] == '1')
+            {
+                dfs(nrow, ncol, vis, grid); // Recursive call to explore the new cell
+            }
+        }
+    }
+
+public:
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int cnt = 0;
+
+        // Iterate through each cell in the grid
+        for (int row = 0; row < n; row++)
+        {
+            for (int col = 0; col < m; col++)
+            {
+                // If the cell is land ('1') and not visited, start DFS from this cell
+                if (!vis[row][col] && grid[row][col] == '1')
+                {
+                    cnt++;
+                    dfs(row, col, vis, grid);
+                }
+            }
+        }
+        return cnt;
+    }
+};
