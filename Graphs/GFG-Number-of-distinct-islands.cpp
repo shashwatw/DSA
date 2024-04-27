ab0c02ce849d9fa699dc03f00694b59bc1b62589
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-04-27 22:28:15 +0530

    Number of distinct islands using BFS

--- Graphs/GFG-Number-of-distinct-islands.cpp
+++ Graphs/GFG-Number-of-distinct-islands.cpp
@@ -1,13 +1,14 @@
 //^ https://practice.geeksforgeeks.org/problems/number-of-distinct-islands/1
 
-//* Intuition
+//! Intuition
 //* We will we storing all the cordinates of the islands in a vector after subtracting them from a base case
 //* Now we will push this vector consisting all the cordinates of islands into as set
 //* As set will only store unique vectors
 //* We can obtain number of unique islands using ste's size
 
-// User function Template for C++
+//& Approach 1 (Using DFS)
 
+//~ CODE:
 class Solution
 {
 private:
@@ -54,6 +55,65 @@ public:
                     vector<pair<int, int>> vec;
                     dfs(i, j, vis, grid, vec, i, j);
                     st.insert(vec);
+                }
+            }
+        }
+        return st.size();
+    }
+};
+
+//^ Approach 2 (Using BFS)
+
+// User function Template for C++
+
+class Solution
+{
+private:
+    void bfs(int r, int c, vector<vector<int>> &matrix, vector<vector<int>> &vis, vector<pair<int, int>> &ans)
+    {
+        vis[r][c] = 1;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        queue<pair<int, int>> q;
+        q.push({r, c});
+        while (!q.empty())
+        {
+            int row = q.front().first;
+            int col = q.front().second;
+            ans.push_back({r - row, c - col});
+            q.pop();
+            int drow[] = {-1, 0, 1, 0};
+            int dcol[] = {0, 1, 0, -1};
+            for (int i = 0; i < 4; i++)
+            {
+                int newrow = row + drow[i];
+                int newcol = col + dcol[i];
+                if (newrow >= 0 and newcol >= 0 and newrow < m and newcol < n and matrix[newrow][newcol] == 1 and !vis[newrow][newcol])
+                {
+                    q.push({newrow, newcol});
+                    vis[newrow][newcol] = 1;
+                }
+            }
+        }
+    }
+
+public:
+    int countDistinctIslands(vector<vector<int>> &matrix)
+    {
+        // code here
+        int m = matrix.size();
+        int n = matrix[0].size();
+        set<vector<pair<int, int>>> st;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (!vis[i][j] and matrix[i][j] == 1)
+                {
+                    vector<pair<int, int>> ans;
+                    bfs(i, j, matrix, vis, ans);
+                    st.insert(ans);
                 }
             }
         }
