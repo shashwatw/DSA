9ddcef0cebf9ad44a81572c5bc871bacc32f8e0b
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-04-28 00:11:57 +0530

    LC 785 Is graph bipartite using DFS

--- Graphs/LC-785-Is-graph-bipartite.cpp
+++ Graphs/LC-785-Is-graph-bipartite.cpp
@@ -62,3 +62,49 @@ public:
         return true;
     }
 };
+
+//^ Approach 2 using DFS
+
+class Solution
+{
+private:
+    bool dfs(int node, int col, vector<int> &color, vector<vector<int>> &graph)
+    {
+        color[node] = col;
+
+        for (auto it : graph[node])
+        {
+            if (color[it] == -1)
+            {
+                if (dfs(it, !col, color, graph) == false)
+                {
+                    return false;
+                }
+            }
+            else if (color[it] == color[node])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    bool isBipartite(vector<vector<int>> &graph)
+    {
+        int V = graph.size();
+        vector<int> color(V, -1);
+
+        for (int i = 0; i < V; ++i)
+        {
+            if (color[i] == -1)
+            {
+                if (dfs(i, 0, color, graph) == false)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
