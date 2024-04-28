537c2060fdda8e1cd414d47b7465be42050a8f5b
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-04-28 18:05:38 +0530

    Topological Sort using Kahn's Algorithm

--- Graphs/GFG-TopoSort-Kahns-Algo.cpp
+++ Graphs/GFG-TopoSort-Kahns-Algo.cpp
@@ -1,17 +1,26 @@
-//// https://practice.geeksforgeeks.org/problems/topological-sort/1
+//^ https://practice.geeksforgeeks.org/problems/topological-sort/1
 
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
+//! Intuition
+//* Firstly we made an array of indegree where we stores indegree by just inc wherever we get adj[node]
+//* Now we initially pushed 0 indegree ones in the queue created
+//* While popping we push them into topo vector to get the linear ordering
+//* Now we decrement indegree of the adjacent nodes of popped element and check if anyone's indegree got 0
+//* If yes then push in q else move forward
+//* When the queue is empty we get a linear topo ordering inside our topo vector
 
-// } Driver Code Ends
+//& TC - O(V+E)
+//& SC - O(2N) = O(N)
+
+//~ CODE (BFS)
 class Solution
 {
 public:
     // Function to return list containing vertices in Topological order.
     vector<int> topoSort(int V, vector<int> adj[])
     {
+        // code here
         int indegree[V] = {0};
+
         for (int i = 0; i < V; i++)
         {
             for (auto it : adj[i])
@@ -21,26 +30,26 @@ public:
         }
 
         queue<int> q;
+
         for (int i = 0; i < V; i++)
         {
             if (indegree[i] == 0)
-            {
                 q.push(i);
-            }
         }
 
         vector<int> topo;
+
         while (!q.empty())
         {
             int node = q.front();
             q.pop();
+
             topo.push_back(node);
-            // node is in your Topo sort
-            // So please remove it from the indegree
 
             for (auto it : adj[node])
             {
                 indegree[it]--;
+
                 if (indegree[it] == 0)
                     q.push(it);
             }
@@ -48,61 +57,3 @@ public:
         return topo;
     }
 };
-
-//{ Driver Code Starts.
-
-/*  Function to check if elements returned by user
- *   contains the elements in topological sorted form
- *   V: number of vertices
- *   *res: array containing elements in topological sorted form
- *   adj[]: graph input
- */
-int check(int V, vector<int> &res, vector<int> adj[])
-{
-
-    if (V != res.size())
-        return 0;
-
-    vector<int> map(V, -1);
-    for (int i = 0; i < V; i++)
-    {
-        map[res[i]] = i;
-    }
-    for (int i = 0; i < V; i++)
-    {
-        for (int v : adj[i])
-        {
-            if (map[i] > map[v])
-                return 0;
-        }
-    }
-    return 1;
-}
-
-int main()
-{
-    int T;
-    cin >> T;
-    while (T--)
-    {
-        int N, E;
-        cin >> E >> N;
-        int u, v;
-
-        vector<int> adj[N];
-
-        for (int i = 0; i < E; i++)
-        {
-            cin >> u >> v;
-            adj[u].push_back(v);
-        }
-
-        Solution obj;
-        vector<int> res = obj.topoSort(N, adj);
-
-        cout << check(N, res, adj) << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends
