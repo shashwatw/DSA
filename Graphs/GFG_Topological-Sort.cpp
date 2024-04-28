7e09b0834079796ebe6cc197ba152fa1ad7f4ab6
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-04-28 15:13:04 +0530

    Topological Sort DFS

--- Graphs/GFG_Topological-Sort.cpp
+++ Graphs/GFG_Topological-Sort.cpp
@@ -1,23 +1,29 @@
-// https://practice.geeksforgeeks.org/problems/topological-sort/1
+//^ https://practice.geeksforgeeks.org/problems/topological-sort/1
 
-// Linear orderung of vertices such that for every directed edge uv, vertex u comes before v in the ordering.
-// For only DAGs
+//* Linear orderung of vertices such that for every directed edge uv, vertex u comes before v in the ordering.
+//* For only DAGs
 
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
+//! INTUTION
+//* Here we are initially checking for disconnected components and then calling dfs for unvisited one
+//* In the dfs call we are going to each and every node till they have nothing further and we push them in bottom up appraoch in stack
+//* Lastly we pop the stack and get the correct linear ordering
 
-// } Driver Code Ends
+//& SC = O(N) + O(N) = O(N)
+//& TC = O(V+E)
+
 class Solution
 {
 private:
-    void dfs(int node, int vis[], stack<int> &st, vector<int> adj[])
+    void dfs(int node, vector<int> adj[], int vis[], stack<int> &st)
     {
         vis[node] = 1;
-        for (auto it : adj[node])
+
+        for (auto adjacentNode : adj[node])
         {
-            if (!vis[it])
-                dfs(it, vis, st, adj);
+            if (!vis[adjacentNode])
+            {
+                dfs(adjacentNode, adj, vis, st);
+            }
         }
         st.push(node);
     }
@@ -26,80 +32,26 @@ public:
     // Function to return list containing vertices in Topological order.
     vector<int> topoSort(int V, vector<int> adj[])
     {
+        // code here
         int vis[V] = {0};
         stack<int> st;
+
         for (int i = 0; i < V; i++)
         {
             if (!vis[i])
             {
-                dfs(i, vis, st, adj);
+                dfs(i, adj, vis, st);
             }
         }
 
         vector<int> ans;
+
         while (!st.empty())
         {
             ans.push_back(st.top());
             st.pop();
         }
+
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-/*  Function to check if elements returned by user
- *   contains the elements in topological sorted form
- *   V: number of vertices
- *   *res: array containing elements in topological sorted form
- *   adj[]: graph input
- */
-int check(int V, vector<int> &res, vector<int> adj[])
-{
-
-    if (V != res.size())
-        return 0;
-
-    vector<int> map(V, -1);
-    for (int i = 0; i < V; i++)
-    {
-        map[res[i]] = i;
-    }
-    for (int i = 0; i < V; i++)
-    {
-        for (int v : adj[i])
-        {
-            if (map[i] > map[v])
-                return 0;
-        }
-    }
-    return 1;
-}
-
-int main()
-{
-    int T;
-    cin >> T;
-    while (T--)
-    {
-        int N, E;
-        cin >> E >> N;
-        int u, v;
-
-        vector<int> adj[N];
-
-        for (int i = 0; i < E; i++)
-        {
-            cin >> u >> v;
-            adj[u].push_back(v);
-        }
-
-        Solution obj;
-        vector<int> res = obj.topoSort(N, adj);
-
-        cout << check(N, res, adj) << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends
