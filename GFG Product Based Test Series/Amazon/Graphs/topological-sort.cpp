4b49f4165f07ba20cb9dda76afd9486c445e3572
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-05-28 15:11:55 +0530

    Topo Sort

---
+++ GFG Product Based Test Series/Amazon/Graphs/topological-sort.cpp
@@ -0,0 +1,45 @@
+//* https://www.geeksforgeeks.org/batch/test-series-bundle/track/amazon-graphs/problem/topological-sort
+
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    int indegree[V] = {0};
+	    
+	    for(int i = 0; i < V; i++)
+	    {
+	        for(auto it : adj[i])
+	        {
+	            indegree[it]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    
+	    for(int  i = 0; i < V; i++)
+	    {
+	        if(indegree[i] == 0) q.push(i);
+	    }
+	    
+	    vector<int> topo;
+	    
+	    while(!q.empty())
+	    {
+	        int node = q.front();
+	        q.pop();
+	        
+	        topo.push_back(node);
+	        
+	        for(auto it : adj[node])
+	        {
+	            indegree[it]--;
+	            
+	            if(indegree[it] == 0) q.push(it);
+	        }
+	    }
+	    return topo;
+	}
+};
