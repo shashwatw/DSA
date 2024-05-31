7ea74a6fcbc3ea5e9b9fa6a1ac276b38074f6376
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-05-31 19:22:34 +0530

    Number of provinces using disjoint set

--- Graphs/GFG_Number-of-Provinces.cpp
+++ Graphs/GFG_Number-of-Provinces.cpp
@@ -1,5 +1,7 @@
 //^ https://practice.geeksforgeeks.org/problems/number-of-provinces/1
 
+//* APPROACH 1 - Using dfs/bfs basic
+
 // SC - O(n) + O(n);
 // TC - O(n) + O(V+2E) {nearly linear TC}
 
@@ -209,6 +211,103 @@ public:
             {
                 cnt++;
                 bfs(i, adjLs, vis);
+            }
+        }
+        return cnt;
+    }
+};
+
+//* APPROACH 2 (using disjoint set) - refer disjoint set first
+
+class DisjointSet
+{
+
+public:
+    vector<int> rank, parent, size;
+
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution
+{
+public:
+    int numProvinces(vector<vector<int>> adj, int V)
+    {
+        // code here
+        DisjointSet ds(V);
+        for (int i = 0; i < V; i++)
+        {
+            for (int j = 0; j < V; j++)
+            {
+                if (adj[i][j] == 1)
+                {
+                    ds.unionBySize(i, j);
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < V; i++)
+        {
+            if (ds.parent[i] == i)
+            {
+                cnt++;
             }
         }
         return cnt;
