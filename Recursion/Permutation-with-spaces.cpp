c847999cb83083f60b5beddcfe003069669c088c
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-06-04 11:41:37 +0530

    Permutation with spaces

---
+++ Recursion/Permutation-with-spaces.cpp
@@ -0,0 +1,42 @@
+//* https://www.geeksforgeeks.org/problems/permutation-with-spaces3627/1
+
+class Solution
+{
+public:
+    void solve(string ip, string op, vector<string> &res)
+    {
+        if (ip.size() == 0)
+        {
+            res.push_back(op);
+            return;
+        }
+
+        string op1 = op;
+        string op2 = op;
+
+        op1 += " ";
+        op1 += ip[0];
+
+        op2 += ip[0];
+
+        ip.erase(ip.begin() + 0);
+
+        solve(ip, op1, res);
+        solve(ip, op2, res);
+    }
+
+    vector<string> permutation(string s)
+    {
+        // Code Here
+        string ip = s;
+        string op = "";
+        vector<string> res;
+
+        op.push_back(ip[0]);
+        ip.erase(ip.begin() + 0);
+
+        solve(ip, op, res);
+
+        return res;
+    }
+};
