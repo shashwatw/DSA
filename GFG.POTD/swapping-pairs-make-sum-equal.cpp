c3faebbcc62e3f27dc782af8e9280c8ce4f37cf9
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-06-05 09:57:27 +0530

    Swapping pairs make sum equal

---
+++ GFG.POTD/swapping-pairs-make-sum-equal.cpp
@@ -0,0 +1,36 @@
+//* https://www.geeksforgeeks.org/problems/swapping-pairs-make-sum-equal4142/1
+
+class Solution
+{
+
+public:
+    int findSwapValues(int a[], int n, int b[], int m)
+    {
+        // Your code goes here
+        set<int> st;
+        int suma = 0;
+        int sumb = 0;
+        for (int i = 0; i < n; i++)
+        {
+            suma += a[i];
+            st.insert(a[i]);
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            sumb += b[i];
+        }
+
+        if ((sumb - suma) % 2 != 0)
+            return -1;
+
+        for (int i = 0; i < m; i++)
+        {
+            if (st.find((suma - sumb) / 2 + b[i]) != st.end())
+            {
+                return 1;
+            }
+        }
+        return -1;
+    }
+};
