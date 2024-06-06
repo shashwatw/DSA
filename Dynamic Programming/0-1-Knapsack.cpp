81059096e5ec73664f11b7f49cada6ca62702675
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-06-06 12:26:53 +0530

    01 Knapsack - Memoization

--- Dynamic Programming/0-1-Knapsack.cpp
+++ Dynamic Programming/0-1-Knapsack.cpp
@@ -21,3 +21,37 @@ public:
         }
     }
 };
+
+//^ APPROACH 2 - MEMOIZATION
+class Solution
+{
+public:
+    // Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n)
+    {
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, -1));
+
+        return knapSackHelper(W, wt, val, n, dp);
+    }
+
+    int knapSackHelper(int W, int wt[], int val[], int n, vector<vector<int>> &dp)
+    {
+        // Your code here
+        //^ APPROACH 2 : MEMOIZATION SOLUTION
+        if (n == 0 || W == 0)
+            return 0; // base condition
+
+        if (dp[n][W] != -1)
+            return dp[n][W];
+
+        if (wt[n - 1] <= W)
+        {
+            return dp[n][W] = max(val[n - 1] + knapSackHelper(W - wt[n - 1], wt, val, n - 1, dp),
+                                  knapSackHelper(W, wt, val, n - 1, dp));
+        }
+        else
+        {
+            return dp[n][W] = knapSackHelper(W, wt, val, n - 1, dp);
+        }
+    }
+};
