ccae9022c1d0c9be75036d65c1201492465faa76
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-06-06 12:59:13 +0530

    01 Knapsack - Tabulation

--- Dynamic Programming/0-1-Knapsack.cpp
+++ Dynamic Programming/0-1-Knapsack.cpp
@@ -56,7 +56,7 @@ public:
     }
 };
 
-//^ Approach 3 - TABULATION METHOD
+//^ APPROACH 3 - TABULATION METHOD
 class Solution
 {
 public:
@@ -107,6 +107,45 @@ public:
                 //     return dp[n][W] = knapSackHelper(W, wt, val,n-1, dp);
                 // }
 
+                if (wt[i - 1] <= j)
+                {
+                    tab[i][j] = max(val[i - 1] + tab[i - 1][j - wt[i - 1]], tab[i - 1][j]);
+                }
+                else
+                {
+                    tab[i][j] = tab[i - 1][j];
+                }
+            }
+        }
+        return tab[n][W];
+    }
+};
+
+//^ APPROACH 3 - TABULATION METHOD (ABOVE CLEAN CODE)
+
+class Solution
+{
+public:
+    // Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n)
+    {
+        int tab[n + 1][W + 1];
+
+        for (int i = 0; i < n + 1; i++)
+        {
+            for (int j = 0; j < W + 1; j++)
+            {
+                if (i == 0 || j == 0) // n == w == 0 base cond
+                {
+                    tab[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            for (int j = 1; j < W + 1; j++)
+            {
                 if (wt[i - 1] <= j)
                 {
                     tab[i][j] = max(val[i - 1] + tab[i - 1][j - wt[i - 1]], tab[i - 1][j]);
