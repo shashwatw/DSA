a0952f215de9b6d6789ac46aaeb39a28f81419cd
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-06-13 11:35:53 +0530

    Minimum number of moves to seat everyone

---
+++ Greedy Algorithms/LC-2037-Min-number-of-moves-to-seat-everyone.cpp
@@ -0,0 +1,19 @@
+//* https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/description/
+
+class Solution
+{
+public:
+    int minMovesToSeat(vector<int> &seats, vector<int> &students)
+    {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+
+        int moves = 0;
+
+        for (int i = 0; i < seats.size(); i++)
+        {
+            moves += abs(seats[i] - students[i]);
+        }
+        return moves;
+    }
+};
