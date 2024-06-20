22f296f849c3e50293b4ea2580f0162303237c90
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-06-20 09:11:01 +0530

    Length of Longest Common Subsequence Memoization

--- Dynamic Programming/length-of-longest-common-subsequence.cpp
+++ Dynamic Programming/length-of-longest-common-subsequence.cpp
@@ -3,6 +3,8 @@
 
 // function to find longest common subsequence
 
+//*               APPROACH 1 ---- RECURSION
+
 class Solution
 {
 public:
@@ -21,5 +23,43 @@ public:
         {
             return max(lcs(n - 1, m, str1, str2), lcs(n, m - 1, str1, str2));
         }
+    }
+};
+
+//*               APPROACH 2 ---- MEMOIZATION
+
+// function to find longest common subsequence
+
+class Solution
+{
+public:
+    int lcshelper(string str1, string str2, int n, int m, vector<vector<int>> &tab)
+    {
+        if (n == 0 || m == 0)
+            return 0;
+
+        if (tab[n][m] != -1)
+        {
+            return tab[n][m];
+        }
+
+        if (str1[n - 1] == str2[m - 1])
+        {
+            return tab[n][m] = 1 + lcshelper(str1, str2, n - 1, m - 1, tab);
+        }
+        else
+        {
+            return tab[n][m] = max(lcshelper(str1, str2, n - 1, m, tab), lcshelper(str1, str2, n, m - 1, tab));
+        }
+    }
+
+    // Function to find the length of longest common subsequence in two strings.
+    int lcs(int n, int m, string str1, string str2)
+    {
+        // your code here
+        vector<vector<int>> tab(n + 1, vector<int>(m + 1, -1));
+
+        // Call the helper function and return its result
+        return lcshelper(str1, str2, n, m, tab);
     }
 };
