b143607f6d511c4f6e438f7845d9b13417ebdae8
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-06-26 12:47:43 +0530

    Morris Traversal Inorder

--- Trees/Leetcode-94-Binary-tree-inorder-traversal-using-morris-traversal.cpp
+++ Trees/Leetcode-94-Binary-tree-inorder-traversal-using-morris-traversal.cpp
@@ -1,5 +1,3 @@
-// https://leetcode.com/problems/binary-tree-inorder-traversal/
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -8,16 +6,20 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
+
+// MORRIS INORDER TRAVERSAL
 class Solution
 {
 public:
     vector<int> inorderTraversal(TreeNode *root)
     {
-        TreeNode *curr = root;
         vector<int> inorder;
+        TreeNode *curr = root;
+
         while (curr != NULL)
         {
             if (curr->left == NULL)
@@ -28,18 +30,20 @@ public:
             else
             {
                 TreeNode *prev = curr->left;
-                while (prev->right && prev->right != curr)
+                while (prev->right != NULL && prev->right != curr)
                 {
                     prev = prev->right;
                 }
+
                 if (prev->right == NULL)
                 {
-                    prev->right = curr;
+                    prev->right = curr; // make a thread from rightmost to curr
                     curr = curr->left;
                 }
                 else
                 {
-                    prev->right = NULL;
+                    prev->right = NULL; // Remove thread if thread already
+                                        // exists and move right of curr
                     inorder.push_back(curr->val);
                     curr = curr->right;
                 }
