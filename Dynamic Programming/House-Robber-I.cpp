e66c7563e9ff78c235ac62018ade825745cd7276
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-06-29 15:14:26 +0530

    House Robber - I -> Memoized

--- Dynamic Programming/House-Robber-I.cpp
+++ Dynamic Programming/House-Robber-I.cpp
@@ -28,3 +28,30 @@ public:
 };
 
 //^ -------------------------------MEMOIZED------------------------------------------------
+
+class Solution
+{
+
+private:
+    int solve(vector<int> &nums, int ind, vector<int> &dp)
+    {
+        if (ind < 0)
+            return 0; // Base case for negative index
+        if (dp[ind] != -1)
+            return dp[ind];
+
+        int pick = nums[ind] + solve(nums, ind - 2, dp);
+        int notPick = 0 + solve(nums, ind - 1, dp);
+
+        // Return the maximum of picking or not picking the current house
+        return dp[ind] = max(pick, notPick);
+    }
+
+public:
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return solve(nums, n - 1, dp); // Start solving from the last house
+    }
+};
