c69b31702af776e9b418acf3e29b74e476a44265
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-06-30 18:17:26 +0530

    LC 62 Unique Path -> Tabulation

--- Dynamic Programming/LC-62-Unique-Paths.cpp
+++ Dynamic Programming/LC-62-Unique-Paths.cpp
@@ -7,6 +7,7 @@
 //* We wrote Base conditions as if it is start node then it is a path and if i or j is going out of matrix do not count as path
 
 //^ -----------------------------------------RECURSIVE-------------------------------------------------
+//& TC - O(2^(m*n)) SC - O(push length)
 
 class Solution
 {
@@ -29,5 +30,94 @@ public:
     {
 
         return solve(m - 1, n - 1);
+    }
+};
+
+//^ -----------------------------------------MEMOIZED-------------------------------------------------
+//& TC - O(N*M) SC-O(N*M) + O((n-1) + (m-1)) --> recur stack space
+
+class Solution
+{
+private:
+    int solve(int i, int j, vector<vector<int>> &dp)
+    {
+        if (i == 0 && j == 0)
+            return 1;
+        if (i < 0 || j < 0)
+            return 0;
+        int up, left;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (i > 0)
+        {
+            if (dp[i - 1][j] != -1)
+            {
+                up = dp[i - 1][j];
+            }
+            else
+            {
+                up = solve(i - 1, j, dp);
+                dp[i - 1][j] = up;
+            }
+        }
+        else
+        {
+            up = 0; // If i == 0, there's no upward path
+        }
+
+        if (j > 0)
+        {
+            if (dp[i][j - 1] != -1)
+            {
+                left = dp[i][j - 1];
+            }
+            else
+            {
+                left = solve(i, j - 1, dp);
+                dp[i][j - 1] = left;
+            }
+        }
+        else
+        {
+            left = 0; // If j == 0, there's no leftward path
+        }
+
+        return dp[i][j] = up + left;
+    }
+
+public:
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(m - 1, n - 1, dp);
+    }
+};
+
+//^ -----------------------------------------TABULATION-------------------------------------------------
+//& TC - O(N*M) SC-O(N*M)
+
+class Solution
+{
+public:
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> tab(m, vector<int>(n, 0));
+
+        // Initialize the first row and first column
+        for (int i = 0; i < m; ++i)
+            tab[i][0] = 1;
+        for (int j = 0; j < n; ++j)
+            tab[0][j] = 1;
+
+        for (int i = 1; i < m; ++i)
+        {
+            for (int j = 1; j < n; ++j)
+            {
+                tab[i][j] = tab[i - 1][j] + tab[i][j - 1];
+            }
+        }
+        return tab[m - 1][n - 1];
     }
 };
