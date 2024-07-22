4a69b5e9ebf9cc6ed5496549a5649379d117d4ee
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-07-22 12:15:30 +0530

    Suset sum problem - Recursive+memoized

--- Dynamic Programming/Subset-sum-problem.cpp
+++ Dynamic Programming/Subset-sum-problem.cpp
@@ -2,6 +2,78 @@
 
 // User function template for C++
 
+//* RECURSIVE APPROACH
+
+class Solution
+{
+private:
+    bool solve(vector<int> &arr, int n, int sum)
+    {
+        if (sum == 0)
+            return true;
+        if (n == 0)
+            return false;
+
+        if (arr[n - 1] <= sum)
+        {
+            return solve(arr, n - 1, sum - arr[n - 1]) || solve(arr, n - 1, sum);
+        }
+        else
+        {
+            return solve(arr, n - 1, sum);
+        }
+    }
+
+public:
+    bool isSubsetSum(vector<int> arr, int sum)
+    {
+        // code here
+        int n = arr.size();
+        return solve(arr, n, sum);
+    }
+};
+
+//* MEMOIZED APPROACH
+
+// User function template for C++
+
+class Solution
+{
+private:
+    bool solve(vector<int> &arr, int n, int sum, vector<vector<int>> &dp)
+    {
+        if (sum == 0)
+            return true;
+        if (n == 0)
+            return false;
+
+        if (dp[n][sum] != -1)
+            return dp[n][sum];
+
+        if (arr[n - 1] <= sum)
+        {
+            dp[n][sum] = solve(arr, n - 1, sum - arr[n - 1], dp) || solve(arr, n - 1, sum, dp);
+        }
+        else
+        {
+            dp[n][sum] = solve(arr, n - 1, sum, dp);
+        }
+        return dp[n][sum];
+    }
+
+public:
+    bool isSubsetSum(vector<int> arr, int sum)
+    {
+        // code here
+        int n = arr.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, -1));
+        return solve(arr, n, sum, dp);
+    }
+};
+
+//* TABULATION METHOD
+
 class Solution
 {
 public:
