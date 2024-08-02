54e6b2bb1e6009d95912ac5d374bb7c2f005201a
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-08-02 21:01:41 +0530

    2134. Minimum Swaps to Group All 1's Together II

---
+++ Arrays/LC-2134-Min-swaps-to-grop-all-1s-together-ii.cpp
@@ -0,0 +1,48 @@
+//* https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together-ii
+
+class Solution
+{
+public:
+    int minSwaps(vector<int> &nums)
+    {
+        int op1 = minSwapsHelper(nums, 0); // Grouping all 0s together
+        int op2 = minSwapsHelper(nums, 1); // Grouping all 1s together
+        return min(op1, op2);
+    }
+
+    int minSwapsHelper(vector<int> &data, int val)
+    {
+        int length = data.size();
+        int totalValCount = 0;
+
+        for (int i = length - 1; i >= 0; i--)
+        {
+            if (data[i] == val)
+                totalValCount++;
+        }
+
+        if (totalValCount == 0 || totalValCount == length)
+            return 0;
+
+        int start = 0, end = 0;
+        int maxValInWindow = 0, currentValInWindow = 0;
+
+        while (end < totalValCount)
+        {
+            if (data[end++] == val)
+                currentValInWindow++;
+        }
+        maxValInWindow = max(maxValInWindow, currentValInWindow);
+
+        while (end < length)
+        {
+            if (data[start++] == val)
+                currentValInWindow--;
+            if (data[end++] == val)
+                currentValInWindow++;
+            maxValInWindow = max(maxValInWindow, currentValInWindow);
+        }
+
+        return totalValCount - maxValInWindow;
+    }
+};
