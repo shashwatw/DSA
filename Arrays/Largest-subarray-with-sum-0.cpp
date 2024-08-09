9d91990611305b441d21d8555402444e5e5e8712
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-08-09 10:19:46 +0530

    Largest Subarray with sum 0

---
+++ Arrays/Largest-subarray-with-sum-0.cpp
@@ -0,0 +1,32 @@
+//* https://www.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
+
+/*You are required to complete this function*/
+
+class Solution
+{
+public:
+    int maxLen(vector<int> &arr, int n)
+    {
+        // Your code here
+        unordered_map<int, int> m;
+        m[0] = 0;
+        int prefixSum = 0;
+        int i = 0;
+        int size = 0;
+        while (i < n)
+        {
+            prefixSum += arr[i];
+            if (m.count(prefixSum))
+            {
+                int j = m[prefixSum];
+                size = max(size, i - j + 1);
+            }
+            else
+            {
+                m[prefixSum] = i + 1;
+            }
+            i++;
+        }
+        return size;
+    }
+};
